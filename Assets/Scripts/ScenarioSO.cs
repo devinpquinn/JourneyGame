0ab2823d0414a9ec120b807cb5ef4b1a0f9d7c7c
Devin <devinpquinn4@gmail.com>
2026-02-15 14:38:02 -0500

    cleanup for complete revamp

--- Assets/Scripts/ScenarioSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewScenario", menuName = "Narrative/Scenario")]
-public class ScenarioSO : ScriptableObject
-{
-    public string scenarioTitle;
-    public DialogNodeSO startNode;
-}
