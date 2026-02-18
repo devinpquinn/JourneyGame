d361abb27eee2315148a275279bddac7f3a7a8c6
Devin <devinpquinn4@gmail.com>
2026-02-18 15:28:51 -0500

    interim

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public static class Hero
+{
+    public static int CurrentHealth = 3;
+    public static int MaxHealth = 3;
+    public static int CurrentMorale = 3;
+    public static int MaxMorale = 3;
+    
+    public static int Strength = 35;
+    public static int Grace = 35;
+    public static int Wisdom = 35;
+    public static int Courage = 35;
+    public static int Luck = 35;
+}
