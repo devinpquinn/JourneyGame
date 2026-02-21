091d89d422c0dd38d5dba3f246d01e20dd907bde
Devin <devinpquinn4@gmail.com>
2026-02-21 13:12:13 -0500

    swapped order of attributes

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -8,8 +8,8 @@ public static class Hero
     public static int MaxMorale = 3;
     
     public static int Strength = 35;
-    public static int Grace = 35;
-    public static int Wisdom = 35;
     public static int Courage = 35;
+    public static int Wisdom = 35;
+    public static int Grace = 35;
     public static int Luck = 35;
 }
